261c1f47943546b70e8a42a582927960a0cc1164
Andrea <andrea.hjelde@gmail.com>
2023-10-20 14:58:46 +0200

    oppgave 1 ferdig

--- src/Components/HomeTask/HomeTask.jsx
+++ src/Components/HomeTask/HomeTask.jsx
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './HomeTask.css';
+import { it } from 'vitest';
 
 function HomeTask({accidents, setLocation}) {
-  // check data structure
-  console.log('Hometask:accidents', accidents)
+
+
+  //SKRIV HVA DETTE GJØR
+  function biggestToSmallest(a,b){
+    b.Ulykkesdato - a.Ulykkesdato
+  }
+
+  //SKRIV HVA DETTE GJØR
+  accidents.sort(biggestToSmallest)
+
+  //SKRIV HVA DETTE GJØR
+  const sortedList = accidents?.sort((a,b) => b.Ulykkesdato - a.Ulykkesdato)
+
+  //Mapper ut objektene i listen "accidents" - returnerer bare kommunenavn og dato
+  const accidentData = sortedList.map((item, index) => {
+    return <li className='accident-list-element' key={index}>{item.Kommunenavn}  {new Date(item.Ulykkesdato).toLocaleDateString()} {new Date(item.Ulykkesdato).toLocaleTimeString()} </li>
+  })
+
+  const accidentDataFull = sortedList.map((item, index) => {
+    console.log(item)
+    return <li className='accident--list-element-expand' key={index}>
+      {item.Antall_enheter}  
+      {item.Fartsgrense} 
+      {item.Fylkenavn} 
+      {item.Føreforhold}
+      {item.Kommunenavn} 
+      {item.Lysforhold}
+      </li>
+  })
+
 
   return (
     <div className="homeTask">
       <header className="homeTask-header">
         HomeTask: Accidents Explorer
       </header>
-
-      <div>Ulykker: {accidents?.length}</div>
-      <div>console.log(accidents);</div>
-
+      <div className='accident-count'>Ulykker: {accidents?.length}</div>
+      <div className='accident-list'>
+        <div >{accidentData}</div>
+        <div >{accidentDataFull}</div>
+      </div>
+      
     </div>
   );
 }
