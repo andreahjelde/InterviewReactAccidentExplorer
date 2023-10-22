b9fa35da02aa38633146977d27bb20e34b1ad123
Andrea <andrea.hjelde@gmail.com>
2023-10-22 18:01:56 +0200

    oppgave 3 ferdig, med unntak av styling

--- src/Components/HomeTask/HomeTask.jsx
+++ src/Components/HomeTask/HomeTask.jsx
@@ -3,6 +3,9 @@ import './HomeTask.css';
 
 function HomeTask({accidents, setLocation}) {
   const [idOfCollapseble, setIdOfCollapseble] = useState(null)
+  const [open, setOpen] = useState(false)
+  const [filterSpeed, setFilter] = useState(null)
+
   //SKRIV HVA DETTE GJØR
   function biggestToSmallest(a,b){
     b.Ulykkesdato - a.Ulykkesdato
@@ -13,9 +16,10 @@ function HomeTask({accidents, setLocation}) {
 
   //SKRIV HVA DETTE GJØR
   const sortedList = accidents?.sort((a,b) => b.Ulykkesdato - a.Ulykkesdato)
+  const filteredList = sortedList.filter(a => filterSpeed === null ||filterSpeed === a.Fartsgrense)
   
     //Mapper ut objektene i listen "accidents" - returnerer bare kommunenavn og dato
-  const accidentData = sortedList.map((item, index) => {
+  const accidentData = filteredList.map((item, index) => {
     
     if( !idOfCollapseble && index === 0 || item.OBJECTID === idOfCollapseble){ // denne skal være åpen
       return <div className='accident-list-element-expand' key={index} onClick={()=>setIdOfCollapseble(item.OBJECTID )}>
@@ -30,6 +34,7 @@ function HomeTask({accidents, setLocation}) {
         <p>Førerforhold: {item.Føreforhold}</p>
         <p>Kommune: {item.Kommunenavn} </p>
         <p>Lysforhold: {item.Lysforhold}</p>
+        
       </div>
     }
 
@@ -42,12 +47,55 @@ function HomeTask({accidents, setLocation}) {
       }
     })
 
+       //SKRIV HVA DETTE GJØR
+    const handleOpen = () => {
+      setOpen(!open)
+    }
+    const handleFilterSelect = (filter)=>{
+      setIdOfCollapseble(null)
+      handleOpen()
+      setFilter(filter)
+      
+    }
+
+   
+      //SKRIV HVA DETTE GJØR
+    const accidentFilter = () => {
+        return <div className='dropdown'>
+        <button onClick={handleOpen}>vis alt</button>
+        {open ? (
+          <ul className='menu'>
+             <li className='menu-item'>
+              <button onClick={()=>{handleFilterSelect(null)}}>Vis alt</button>
+            </li>
+            <li className='menu-item'>
+              <button onClick={()=>{handleFilterSelect("110")}}>110</button>
+            </li>
+            <li className='menu-item'>
+              <button onClick={()=>{handleFilterSelect("100")}}>100</button>
+            </li>
+            <li className='menu-item'>
+              <button onClick={()=>{handleFilterSelect("90")}}>90</button>
+            </li>
+            <li className='menu-item'>
+              <button onClick={()=>{handleFilterSelect("80")}}>80</button>
+            </li>
+          </ul>
+        ):  null}
+          </div>
+        
+    }
+
   return (
     <div className="homeTask">
       <header className="homeTask-header">
         HomeTask: Accidents Explorer
       </header>
-      <div className='accident-count'>Ulykker: {accidents?.length}</div>
+      <div className='accident-count'>Ulykker: {filteredList?.length}</div>
+      <div className='accident-filter'>
+        <p>Filter:</p> 
+        {accidentFilter()}
+      </div>
       <div className='accident-list'>
         <div >{accidentData}</div>
       </div>
